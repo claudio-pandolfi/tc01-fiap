bff89cedd190b6db05ed5f4ab1d5ff7b98dc5400
Claudio Pandolfi <claudio.pandolfi@gmail.com>
2025-06-02 18:42:03 -0300

    Deploy Heroku

--- code/app/models/__init__.py
+++ code/app/models/__init__.py
@@ -1,4 +1,5 @@
 from mongoengine import connect
 
 def register_db_connection(app):
-    connect(app.config['MONGODB_DATABASE'], host=app.config['MONGODB_HOST'], port=app.config['MONGODB_PORT'])
+    # connect(app.config['MONGODB_DATABASE'], host=app.config['MONGODB_HOST'], port=app.config['MONGODB_PORT'])
+    connect(host=app.config['MONGODB_HOST'])
