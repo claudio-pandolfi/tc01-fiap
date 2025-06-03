a2afb166bf37acff0c990447884b1a55acbb01ed
Claudio Pandolfi <claudio.pandolfi@gmail.com>
2025-06-03 08:28:05 -0300

    Ajuste conexao do redis

--- code/app/resources/__init__.py
+++ code/app/resources/__init__.py
@@ -3,5 +3,5 @@ from redis import Redis
 from rq import Queue
 from app.config import Config
 
-redis_conn = Redis(host=Config().__dict__['REDIS_URL'], port=6379, db=0)
+redis_conn = Redis.from_url(url=Config().__dict__['REDIS_URL'])
 queue = Queue('scraping', connection=redis_conn)
